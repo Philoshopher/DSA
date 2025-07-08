1cc609540ba81b3da1559e867b6a509ed690dc5f
Philoshopher <prathmeshchauhan2304@gmail.com>
2025-07-08 18:56:19 +0530

    Solved sum of number using while loop.

---
+++ SumOfNumbers(while loop).js
@@ -0,0 +1,19 @@
+// Sum of all the digits in the given number value using while loop
+// for ex. n = 1234 the ans ==  1 + 2+ 3+ 4 = 10
+
+function getSum(n) {
+  if (isNaN(n)) {
+    console.log('Invalid Input!!');
+  } else {
+    let ans = 0;
+    while (n > 0) {
+      let tmp = 0;
+      tmp = +Math.floor(n % 10);
+      ans = ans + tmp;
+      n = Math.floor(n / 10);
+    }
+    console.log(ans);
+  }
+}
+
+getSum();
